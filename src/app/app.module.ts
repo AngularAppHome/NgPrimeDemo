a201514e8d588dc4958aaed231313351c59885f7
Deepak Srivastav <srivastv.deepak@gmail.com>
2018-08-15 13:43:07 +0530

    Added navigation and added some core css and images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { appRouterModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, appRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
